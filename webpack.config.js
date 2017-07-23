0d6bf9a12c5cb747f489580613b65a6626f2ec26
Michael Rubanov <mrubanov@gmail.com>
2017-07-23 03:29:09 +0300

    addin copy of resources/assets content to dist/

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
 const OUT_DIR = 'dist';
 const path = require('path');
+let CopyWebpackPlugin = require('copy-webpack-plugin');
+
+const copyOptions = [{from : "resources/assets/"}];
 
  module.exports = {
      entry: './src/index.js',
@@ -7,6 +10,9 @@ const path = require('path');
          path: path.resolve(__dirname, OUT_DIR),
          filename: 'app.bundle.js',
      },
+     plugins: [
+       new CopyWebpackPlugin(copyOptions),
+     ],
      module: {
          loaders: [{
              test: /\.js$/,
